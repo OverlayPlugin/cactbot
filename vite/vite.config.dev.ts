11b778e8068b7acbb984f7d5d15578661e145128
Maiko Tan <maiko.tan.coding@gmail.com>
2024-05-31 12:00:06 +0800

    build: add timerbar_test.html into dev config

--- vite/vite.config.dev.ts
+++ vite/vite.config.dev.ts
@@ -5,6 +5,11 @@ import baseOptions from './vite.config.base';
 export default mergeConfig<UserConfig, UserConfig>(baseOptions, {
   build: {
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        timerbarTest: 'ui/test/timerbar_test.html',
+      },
+    },
   },
   server: {
     port: 8080,
