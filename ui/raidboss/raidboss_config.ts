b988bf85b82fcae6d956b9ab2f9180cb2a53e002
Souma-Sumire <553469159@qq.com>
2024-06-21 10:08:15 +0800

    warning user

--- ui/raidboss/raidboss_config.ts
+++ ui/raidboss/raidboss_config.ts
@@ -2541,6 +2541,12 @@ const templateOptions: OptionsTemplate = {
       },
       type: 'multiline',
       default: '',
+      comment: {
+        en:
+          '<strong style="color:red;>WARNING!</strong>Importing remote JavaScript files can expose your device to risks. These files might steal your information or damage your device. Make sure to import files only from trusted developers.',
+        cn:
+          '<strong style="color:red;">警告！</strong>导入远程 JavaScript 文件可能会让你的设备暴露在风险中。这些文件可能会窃取你的信息或造成设备损坏。请确保只从信任的开发者处导入文件。',
+      },
     },
   ],
 };
