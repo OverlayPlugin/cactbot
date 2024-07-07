ec233ff749928d32dc72606b2e302a22480d7d71
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-07 15:06:52 +0800

    add stacks for living muse

--- ui/jobs/components/pct.ts
+++ ui/jobs/components/pct.ts
@@ -6,6 +6,8 @@ import { BaseComponent, ComponentInterface } from './base';
 export class PCTComponent extends BaseComponent {
   whitePaint: ResourceBox;
   paletteGauge: ResourceBox;
+  livingCanvasStacks: HTMLDivElement;
+
   constructor(o: ComponentInterface) {
     super(o);
 
@@ -15,6 +17,14 @@ export class PCTComponent extends BaseComponent {
     this.paletteGauge = this.bars.addResourceBox({
       classList: ['pct-color-palettegauge'],
     });
+    this.livingCanvasStacks = document.createElement('div');
+    this.livingCanvasStacks.id = 'pct-stacks-living';
+    this.livingCanvasStacks.classList.add('stacks');
+    for (let i = 0; i < 4; i++) {
+      const stack = document.createElement('div');
+      this.livingCanvasStacks.appendChild(stack);
+    }
+    this.bars.addJobBarContainer().appendChild(this.livingCanvasStacks);
 
     this.reset();
   }
@@ -27,11 +37,38 @@ export class PCTComponent extends BaseComponent {
       this.whitePaint.parentElement?.classList.remove('pulse');
     }
     this.paletteGauge.innerText = jobDetail.paletteGauge.toString();
+
+    // Light up the rendered stacks for the current living canvas objects.
+    livingCanvasObjects.forEach((obj, i) => {
+      if (jobDetail.depictions?.includes(obj as typeof jobDetail.depictions[number])) {
+        this.livingCanvasStacks.children[i]?.classList.add('active');
+      } else if (obj === jobDetail.creatureMotif) {
+        this.livingCanvasStacks.children[i]?.classList.add('active', 'pulse');
+      } else {
+        this.livingCanvasStacks.children[i]?.classList.remove('active', 'pulse');
+      }
+    });
+    if (!jobDetail.creatureMotif || jobDetail.creatureMotif === 'None') {
+      for (let i = 0; i < livingCanvasObjects.length; i++) {
+        const obj = livingCanvasObjects[i];
+        if (jobDetail.depictions?.includes(obj as typeof jobDetail.depictions[number])) {
+          continue;
+        }
+        this.livingCanvasStacks.children[i]?.classList.add('pulse');
+        break;
+      }
+    }
   }
 
   override reset(): void {
     super.reset();
     this.paletteGauge.innerText = '';
     this.whitePaint.innerText = '';
+    this.livingCanvasStacks.childNodes?.forEach((stack) => {
+      if (stack instanceof HTMLElement)
+        stack.classList.remove('active', 'pulse');
+    });
   }
 }
+
+const livingCanvasObjects = ['Pom', 'Wing', 'Claw', 'Maw'] as const;
