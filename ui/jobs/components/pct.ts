3abeda15caf94ec3daa86cae93f2f0d1f2c8f6d3
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-07 14:26:29 +0800

    add pulse effect

--- ui/jobs/components/pct.ts
+++ ui/jobs/components/pct.ts
@@ -21,6 +21,11 @@ export class PCTComponent extends BaseComponent {
 
   override onJobDetailUpdate(jobDetail: JobDetail['PCT']): void {
     this.whitePaint.innerText = jobDetail.paint.toString();
+    if (this.whitePaint.innerText === '5') {
+      this.whitePaint.parentElement?.classList.add('pulse');
+    } else {
+      this.whitePaint.parentElement?.classList.remove('pulse');
+    }
     this.paletteGauge.innerText = jobDetail.paletteGauge.toString();
   }
 
