95b1c57704453bb01b050713fe42fa6e9d3a0af2
Wexx <86693821+wexxlee@users.noreply.github.com>
2024-07-07 11:31:27 -0700

    initial untested stuff

--- ui/raidboss/data/07-dt/trial/zoraal-ja-ex.ts
+++ ui/raidboss/data/07-dt/trial/zoraal-ja-ex.ts
@@ -1,6 +1,13 @@
+import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
+
+// TO DO:
+// - Regicidal Rage - add appropriate tank call
+// - Vollok/Sync - add safe tile(s) callout?
+// - Sync/Half Full - call safe boss half
+// - Half Circuit - call safe side
 
 const triggerSet: TriggerSet<RaidbossData> = {
   id: 'EverkeepExtreme',
@@ -10,45 +17,100 @@ const triggerSet: TriggerSet<RaidbossData> = {
     {
       id: 'Zoraal Ja Ex Forward Half Right Sword',
       type: 'StartsUsing',
-      netRegex: { id: '937B', capture: false },
+      netRegex: { id: '937B', source: 'Zoraal Ja', capture: false },
       alertText: (_data, _matches, output) => output.frontRight!(),
       outputStrings: {
         frontRight: {
-          en: 'In front of boss + right side of boss',
+          en: 'Front + Boss\'s Right',
         },
       },
     },
     {
       id: 'Zoraal Ja Ex Forward Half Left Sword',
       type: 'StartsUsing',
-      netRegex: { id: '937C', capture: false },
-      alertText: (_data, _matches, output) => output.frontRight!(),
+      netRegex: { id: '937C', source: 'Zoraal Ja', capture: false },
+      alertText: (_data, _matches, output) => output.frontLeft!(),
       outputStrings: {
-        frontRight: {
-          en: 'In front of boss + left side of boss',
+        frontLeft: {
+          en: 'Front + Boss\'s Left',
         },
       },
     },
     {
       id: 'Zoraal Ja Ex Backward Half Right Sword',
       type: 'StartsUsing',
-      netRegex: { id: '937D', capture: false },
-      alertText: (_data, _matches, output) => output.frontRight!(),
+      netRegex: { id: '937D', source: 'Zoraal Ja', capture: false },
+      alertText: (_data, _matches, output) => output.backRight!(),
       outputStrings: {
-        frontRight: {
-          en: 'Behind boss + left side of boss',
+        backRight: {
+          en: 'Behind + Boss\'s Left',
         },
       },
     },
     {
       id: 'Zoraal Ja Ex Backward Half Left Sword',
       type: 'StartsUsing',
-      netRegex: { id: '937E', capture: false },
-      alertText: (_data, _matches, output) => output.frontRight!(),
+      netRegex: { id: '937E', source: 'Zoraal Ja', capture: false },
+      alertText: (_data, _matches, output) => output.backLeft!(),
       outputStrings: {
-        frontRight: {
-          en: 'Behind boss + right side of boss',
+        backLeft: {
+          en: 'Behind + Boss\'s Right',
         },
+      },
+    },
+    {
+      id: 'Zoraal Ja Ex Actualize',
+      type: 'StartsUsing',
+      netRegex: { id: '9398', source: 'Zoraal Ja', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      // FIX ME
+      id: 'Zoraal Ja Ex Regicidal Rage',
+      type: 'StartsUsing',
+      netRegex: { id: '993C', source: 'Zoraal Ja', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Buster tethers (fix me)',
+        },
+      },
+    },
+    {
+      id: 'Zoraal Ja Ex Dawn of an Age',
+      type: 'StartsUsing',
+      netRegex: { id: '9397', source: 'Zoraal Ja', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Zoraal Ja Ex Sync',
+      type: 'StartsUsing',
+      netRegex: { id: '9359', source: 'Zoraal Ja', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Avoid Swords (+Halfroom cleave?)',
+        },
+      },
+    },
+    // FIX ME
+    {
+      id: 'Zoraal Ja Ex Half Circuit',
+      type: 'StartsUsing',
+      netRegex: { id: ['936B', '936C'], source: 'Zoraal Ja', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Dodge half-room cleave + in/out',
+        },
+      },
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'en',
+      'replaceText': {
+        'Forward Edge/Backward Edge': 'Forward/Backward Edge',
       },
     },
   ],
