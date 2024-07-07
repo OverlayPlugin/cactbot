33697eae85c0eba9c04d29c3dbf255ca94af1fbb
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-07 14:02:41 +0800

    jobs: implement PCT job component

--- ui/jobs/components/pct.ts
+++ ui/jobs/components/pct.ts
@@ -1,7 +1,32 @@
+import { JobDetail } from '../../../types/event';
+import { ResourceBox } from '../bars';
+
 import { BaseComponent, ComponentInterface } from './base';
 
 export class PCTComponent extends BaseComponent {
+  whitePaint: ResourceBox;
+  paletteGauge: ResourceBox;
   constructor(o: ComponentInterface) {
     super(o);
+
+    this.whitePaint = this.bars.addResourceBox({
+      classList: ['pct-color-whitepaint'],
+    });
+    this.paletteGauge = this.bars.addResourceBox({
+      classList: ['pct-color-palettegauge'],
+    });
+
+    this.reset();
+  }
+
+  override onJobDetailUpdate(jobDetail: JobDetail['PCT']): void {
+    this.whitePaint.innerText = jobDetail.paint.toString();
+    this.paletteGauge.innerText = jobDetail.paletteGauge.toString();
+  }
+
+  override reset(): void {
+    super.reset();
+    this.paletteGauge.innerText = '';
+    this.whitePaint.innerText = '';
   }
 }
