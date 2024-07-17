cef067c61dd0345a22f6f7c9cb6ecafab4336295
Echoring <68432572+Echoring@users.noreply.github.com>
2024-07-17 17:50:23 +0800

    revert GCD init

--- ui/jobs/components/blm.ts
+++ ui/jobs/components/blm.ts
@@ -86,7 +86,6 @@ export class BLMComponent extends BaseComponent {
     });
 
     this.reset();
-    this.onStatChange({ gcdSpell: this.player.gcdSpell });
   }
 
   override onUseAbility(id: string): void {
