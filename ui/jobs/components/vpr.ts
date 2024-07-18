258fddc633a8fe2ca7ed5e67e57c937d0c82e29e
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-18 10:56:32 +0800

    fix: move noxious gnash to mob effect

--- ui/jobs/components/vpr.ts
+++ ui/jobs/components/vpr.ts
@@ -41,9 +41,6 @@ export class VPRComponent extends BaseComponent {
         this.player.speedBuffs.swiftscaled = true;
         this.swiftscaledTimer.duration = Number(matches.duration) || 0;
         break;
-      case EffectId.NoxiousGnash:
-        this.noxiousGnashTimer.duration = Number(matches.duration) || 0;
-        break;
       case EffectId.HuntersInstinct:
         this.huntersInstinctTimer.duration = Number(matches.duration) || 0;
         break;
@@ -56,12 +53,27 @@ export class VPRComponent extends BaseComponent {
         this.player.speedBuffs.swiftscaled = false;
         this.swiftscaledTimer.duration = 0;
         break;
-      case EffectId.NoxiousGnash:
-        this.noxiousGnashTimer.duration = 0;
-        break;
       case EffectId.HuntersInstinct:
         this.huntersInstinctTimer.duration = 0;
         break;
+    }
+  }
+
+  override onMobGainsEffectFromYou(id: string, matches: PartialFieldMatches<'GainsEffect'>): void {
+    switch (id) {
+      case EffectId.NoxiousGnash: {
+        // FIXME:
+        // Death's Design can be different duration on multiple target,
+        // and this condition will only monitor the longest one.
+        // If you defeat a target with longer Death's Design duration remains
+        // and move to a new or shorter duration target,
+        // This timer will not work well until new Death's Design duration exceed timer.
+        // For the same reason, timer will not reset when target with debuff is defeated.
+        const duration = parseFloat(matches.duration ?? '0') || 0;
+        if (this.noxiousGnashTimer.value < duration)
+          this.noxiousGnashTimer.duration = duration;
+        break;
+      }
     }
   }
 
