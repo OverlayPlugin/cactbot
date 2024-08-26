d1c79ff8b32961cefe8db05b05cd4131a10fad6a
Wexx <86693821+wexxlee@users.noreply.github.com>
2024-08-25 21:14:48 -0700

    add heritage found(ation)

--- ui/raidboss/data/07-dt/hunts/heritage_found.ts
+++ ui/raidboss/data/07-dt/hunts/heritage_found.ts
@@ -1,3 +1,6 @@
+import Conditions from '../../../../../resources/conditions';
+import Outputs from '../../../../../resources/outputs';
+import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
@@ -5,18 +8,158 @@ import { TriggerSet } from '../../../../../types/trigger';
 // TODO: Add triggers for Atticus the Primogenitor (S-Rank)
 
 export interface Data extends RaidbossData {
+  magnetronDebuff?: 'positive' | 'negative';
+  storedShockSafe?: 'intercards' | 'cardinals';
 }
 
 const triggerSet: TriggerSet<Data> = {
   id: 'HeritageFound',
   zoneId: ZoneId.HeritageFound,
-  initData: () => ({
-  }),
   triggers: [
     // ****** A-RANK: Heshuala ****** //
+    {
+      id: 'Hunt Heshuala Electrical Overload',
+      type: 'StartsUsing',
+      netRegex: { id: '98C1', source: 'Heshuala' },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Hunt Heshuala Stored Shock Early',
+      type: 'GainsEffect',
+      // F89: Shocking Cross (+ cleave, intercards safe)
+      // F8A: X Marks the Shock (x cleave, cardinals safe)
+      netRegex: { effectId: ['F89', 'F8A'], target: 'Heshuala' },
+      infoText: (data, matches, output) => {
+        const safe = matches.effectId === 'F89' ? 'intercards' : 'cardinals';
+        data.storedShockSafe = safe;
+        return output[safe]!();
+      },
+      outputStrings: {
+        cardinals: {
+          en: '(cardinals later)',
+        },
+        intercards: {
+          en: '(intercards later)',
+        },
+      },
+    },
+    {
+      id: 'Hunt Heshuala Stored Shock Now',
+      type: 'LosesEffect',
+      // F8B: Electrical Charge (LosesEffect happens right before the Stored Shock resolves)
+      netRegex: { effectId: 'F8B', target: 'Heshuala', capture: false },
+      alertText: (data, _matches, output) => {
+        const safe = data.storedShockSafe;
+        if (safe !== undefined)
+          return output[safe]!();
+      },
+      run: (data) => delete data.storedShockSafe,
+      outputStrings: {
+        cardinals: Outputs.cardinals,
+        intercards: Outputs.intercards,
+      },
+    },
 
     // ****** A-RANK: Urna Variabilis ****** //
+    {
+      id: 'Hunt Urna Proximity Plasma',
+      type: 'StartsUsing',
+      netRegex: { id: '98C2', source: 'Urna Variabilis', capture: false },
+      response: Responses.getOut('info'),
+    },
+    {
+      id: 'Hunt Urna Ring Lightning',
+      type: 'StartsUsing',
+      netRegex: { id: '98C3', source: 'Urna Variabilis', capture: false },
+      response: Responses.getIn('info'),
+    },
+    {
+      id: 'Hunt Urna Magnetron',
+      type: 'StartsUsing',
+      netRegex: { id: '98C4', source: 'Urna Variabilis' },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Hunt Urna Thunderous Shower',
+      type: 'StartsUsing',
+      netRegex: { id: '98CB', source: 'Urna Variabilis' },
+      response: Responses.stackMarkerOn(),
+    },
+    {
+      id: 'Hunt Urna Electrowave',
+      type: 'StartsUsing',
+      netRegex: { id: '98CC', source: 'Urna Variabilis' },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Hunt Urna Magnetron Debuff',
+      type: 'GainsEffect',
+      // FE7: Positive Charge, FE8: Negative Charge
+      netRegex: { effectId: ['FE7', 'FE8'], source: 'Urna Variabilis' },
+      condition: Conditions.targetIsYou(),
+      run: (data, matches) =>
+        data.magnetronDebuff = matches.effectId === 'FE7' ? 'positive' : 'negative',
+    },
+    {
+      id: 'Hunt Urna Magnetoplasma',
+      type: 'StartsUsing',
+      // 98C5: Boss Positive, 98C7: Boss Negative
+      netRegex: { id: ['98C5', '98C7'], source: 'Urna Variabilis' },
+      durationSeconds: 8,
+      alertText: (data, matches, output) => {
+        const bossMagnet = matches.id === '98C5' ? 'positive' : 'negative';
+        const myMagnet = data.magnetronDebuff;
+        if (myMagnet === undefined)
+          return output.out!();
 
+        if (bossMagnet === myMagnet)
+          return output.combo!({ dir: output.out!(), magnet: output.repel!() });
+        return output.combo!({ dir: output.out!(), magnet: output.attract!() });
+      },
+      run: (data) => delete data.magnetronDebuff,
+      outputStrings: {
+        out: Outputs.out,
+        repel: {
+          en: 'Forced knockback',
+        },
+        attract: {
+          en: 'Forced draw-in',
+        },
+        combo: {
+          en: '${dir} (${magnet})',
+        },
+      },
+    },
+    {
+      id: 'Hunt Urna Magnetoring',
+      type: 'StartsUsing',
+      // 98C6: Boss Positive, 98C8: Boss Negative
+      netRegex: { id: ['98C6', '98C8'], source: 'Urna Variabilis' },
+      durationSeconds: 8,
+      alertText: (data, matches, output) => {
+        const bossMagnet = matches.id === '98C6' ? 'positive' : 'negative';
+        const myMagnet = data.magnetronDebuff;
+        if (myMagnet === undefined)
+          return output.in!();
+
+        if (bossMagnet === myMagnet)
+          return output.combo!({ dir: output.in!(), magnet: output.repel!() });
+        return output.combo!({ dir: output.in!(), magnet: output.attract!() });
+      },
+      run: (data) => delete data.magnetronDebuff,
+      outputStrings: {
+        in: Outputs.in,
+        repel: {
+          en: 'Forced knockback',
+        },
+        attract: {
+          en: 'Forced draw-in',
+        },
+        combo: {
+          en: '${dir} (${magnet})',
+        },
+      },
+    },
     // ****** S-RANK: Atticus the Primogenitor ****** //
   ],
   timelineReplace: [],
