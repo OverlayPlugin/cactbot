95ac86e254748c2ab1fc450ab3f521163f32a61a
Wexx <86693821+wexxlee@users.noreply.github.com>
2024-09-07 14:13:10 -0700

    add todo

--- ui/raidboss/data/07-dt/hunts/ss_rank.ts
+++ ui/raidboss/data/07-dt/hunts/ss_rank.ts
@@ -5,6 +5,8 @@ import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
 
+// TODO: Add minions?
+
 export type Data = RaidbossData;
 
 const triggerSet: TriggerSet<Data> = {
