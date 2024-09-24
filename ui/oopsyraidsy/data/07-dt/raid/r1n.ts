3e814f396d7178d3670d25596d145f043b95f891
Wexx <86693821+wexxlee@users.noreply.github.com>
2024-09-23 20:18:58 -0700

    cleanup

--- ui/oopsyraidsy/data/07-dt/raid/r1n.ts
+++ ui/oopsyraidsy/data/07-dt/raid/r1n.ts
@@ -2,7 +2,9 @@ import ZoneId from '../../../../../resources/zone_id';
 import { OopsyData } from '../../../../../types/data';
 import { OopsyTriggerSet } from '../../../../../types/oopsy';
 
-// TODO: Add triggers to collect/report players who miss Clawful & Overshadow stacks
+// TODO:
+//   - Add trigger for missed Clawful (stack)
+//   - Add trigger for missed Oveershadow (stack)
 
 export type Data = OopsyData;
 
@@ -15,8 +17,8 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'R1N One-two Paw Right Second': '930D', // half-room cleaves (L->R)
     'R1N Black Cat Crossing First': '9311', // first set of cone cleaves
     'R1N Black Cat Crossing Second': '9312', // second set of cone cleaves
-    'R1N Mouser': '94A5', // Hit on tile (but not yet falling)
-    'R1N Predaceous Pounce Line': '9964', // line aoe to telegraph jump
+    'R1N Mouser': '94A5', // Tile hit (but not yet broken)
+    'R1N Predaceous Pounce Line': '9964', // line AoE telegraphing next jump
     'R1N Predaceous Pounce Jump': '9931', // circle AoE
     'R1N Leaping One-two Paw East Right': '9324', // jump + arena cleaves
     'R1N Leaping One-two Paw East Left': '9325', // jump + arena cleaves
@@ -26,7 +28,7 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'R1N Leaping Black Cat Crossing Second': '932D', // jump + second set of cone cleaves
   },
   shareWarn: {
-    'R1N Grimalkin Gale': '933F', // spreads
+    'R1N Grimalkin Gale': '933F', // spread circle AoEs
   },
 };
 
