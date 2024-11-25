55889c48a128f73a530d7a5bff990b91d9c186b0
Jonathan Garber <linkthevaliant@gmail.com>
2024-11-24 22:58:09 -0500

    Add Prishe triggers
    asdf

---
+++ ui/raidboss/data/07-dt/alliance/jeuno-first-walk.ts
@@ -0,0 +1,182 @@
+import Conditions from '../../../../../resources/conditions';
+import Outputs from '../../../../../resources/outputs';
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+// TODO: Math out the explosion patterns on Banishga IV and call in/out pattern.
+// TODO: Figure out the targeting on Prishe's Asuran Fists (repeated stack marker)
+
+export type Data = RaidbossData;
+
+const prishePunchDelays: string[] = [
+  '9FE8',
+  '9FF6',
+];
+
+const triggerSet: TriggerSet<Data> = {
+  id: 'Jeuno: The First Walk',
+  zoneId: ZoneId.JeunoTheFirstWalk,
+  timelineFile: 'jeuno-first-walk.txt',
+  initData: () => {
+    return {
+
+    };
+  },
+  triggers: [
+    {
+      id: 'Jeuno First Walk Prishe Banishga',
+      type: 'StartsUsing',
+      netRegex: { id: '9FE7', source: 'Prishe Of The Distant Chains', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Jeuno First Walk Prishe Knuckle Sandwich',
+      type: 'StartsUsing',
+      netRegex: { id: ['9FE8', '9FE9', '9FEA'], source: 'Prishe Of The Distant Chains', capture: true },
+      // The player is intended to count the number of "wait for it" emotes from Prishe.
+      // Delay to match how many she would call per ability.
+      // (It's not necessary to delay past 6 seconds,
+      // as at that point the player knows it's 2/3 emotes)
+
+      // 9FE8: Inner circle, 1x emote
+      // 9FE9: Mid circle, 2x emote
+      // 9FEA: Big circle, 3x emote, but its delay is the same as
+      delaySeconds: (_data, matches) => {
+        const delay = prishePunchDelays.includes(matches.id) ? 4 : 6;
+        return delay;
+      },
+      durationSeconds: (_data, matches) => {
+        // The total cast time is 11.7 seconds
+        const duration = prishePunchDelays.includes(matches.id) ? 7.7 : 5.7;
+        return duration;
+      },
+      infoText: (_data, matches, output) => {
+        if (matches.id === '9FE8')
+          return output.smallCircle!();
+        if (matches.id === '9FE9')
+          return output.midCircle!();
+        if (matches.id === '9FEA')
+          return output.bigCircle!();
+        return output.unknownCircle!();
+      },
+      outputStrings: {
+        smallCircle: {
+          en: 'Outside small circle => in',
+        },
+        midCircle: {
+          en: 'Outside mid circle => in',
+        },
+        bigCircle: {
+          en: 'Outside big circle => in',
+        },
+        unknownCircle: Outputs.unknown,
+      },
+    },
+    {
+      id: 'Jeuno First Walk Prishe Nullifying Dropkick',
+      type: 'HeadMarker',
+      netRegex: { id: '023A', capture: true },
+      response: Responses.sharedTankBuster(),
+    },
+    {
+      id: 'Jeuno First Walk Prishe Banish Storm',
+      type: 'Ability', // This resolves before the AoEs even appear
+      netRegex: { id: '9FF2', source: 'Prishe Of The Distant Chains', capture: false },
+      alertText: (_data, _matches, output) => output.avoidCircles!(),
+      outputStrings: {
+        avoidCircles: {
+          en: 'Avoid radiating circles',
+        },
+      },
+    },
+    {
+      id: 'Jeuno First Walk Prishe Holy',
+      type: 'HeadMarker',
+      netRegex: { id: '00D7', capture: true },
+      condition: Conditions.targetIsYou(),
+      response: Responses.spread(),
+    },
+    {
+      id: 'Jeuno First Walk Prishe Auroral Uppercut',
+      type: 'StartsUsing',
+      netRegex: { id: ['9FF6', '9FF7', '9FF8'], source: 'Prishe Of The Distant Chains', capture: true },
+      // The player is intended to count the number of "wait for it" emotes from Prishe.
+      // Delay to match how many she would call per ability.
+      // (It's not necessary to delay past 6 seconds,
+      // as at that point the player knows it's 2/3 emotes)
+
+      // 9FF6: Short knockback, 1x emote
+      // 9FF7: Mid knockback, 2x emote
+      // 9FF8: Big knockback, 3x emote
+      delaySeconds: (_data, matches) => {
+        const delay = prishePunchDelays.includes(matches.id) ? 4 : 6;
+        return delay;
+      },
+      durationSeconds: (_data, matches) => {
+        // The total cast time is 11.7 seconds
+        const duration = prishePunchDelays.includes(matches.id) ? 7.7 : 5.7;
+        return duration;
+      },
+      infoText: (_data, matches, output) => {
+        if (matches.id === '9FF6')
+          return output.shortKnockback!();
+        if (matches.id === '9FF7')
+          return output.midKnockback!();
+        if (matches.id === '9FF8')
+          return output.bigKnockback!();
+        return output.unknownKnockback!();
+      },
+      outputStrings: {
+        shortKnockback: {
+          en: 'Aim short knockback',
+        },
+        midKnockback: {
+          en: 'Aim mid knockback',
+        },
+        bigKnockback: {
+          en: 'Aim big knockback',
+        },
+        unknownKnockback: Outputs.unknown,
+      },
+    },
+    {
+      id: 'Jeuno First Walk Prishe Banishga IV',
+      type: 'StartsUsing',
+      netRegex: { id: '9FFA', source: 'Prishe Of The Distant Chains', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Jeuno First Walk Prishe Banishga Orbs',
+      type: 'Ability',
+      netRegex: { id: '9FFA', source: 'Prishe Of The Distant Chains', capture: false },
+      durationSeconds: 6,
+      suppressSeconds: 1,
+      alertText: (_data, _matches, output) => output.avoidOrbs!(),
+      outputStrings: {
+        avoidOrbs: {
+          en: 'Avoid large orbs',
+        },
+      },
+    },
+    {
+      // This is self-targeted, and there's no clear information currently on who gets the stack marker.
+      id: 'Jeuno First Walk Prishe Asuran Fists',
+      type: 'StartsUsing',
+      netRegex: { id: '9FFC', source: 'Prishe Of The Distant Chains', capture: false },
+      durationSeconds: 6,
+      response: Responses.stackMarker(),
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'en',
+      'replaceText': {
+
+      },
+    },
+  ],
+};
+
+export default triggerSet;
