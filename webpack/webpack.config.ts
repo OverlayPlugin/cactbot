8b5919abcae3b73d3cc73e0f13eedd706ff9fb83
Maiko Tan <maiko.tan.coding@gmail.com>
2024-12-05 11:41:11 +0800

    better (?) solution for tsconfig

--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -88,7 +88,7 @@ export default (
   ];
 
   if (!isCI)
-    plugins.unshift(new ForkTsCheckerWebpackPlugin({ typescript: { configFile: 'tsconfig.project.json' } }));
+    plugins.unshift(new ForkTsCheckerWebpackPlugin());
 
   return {
     entry: entries,
