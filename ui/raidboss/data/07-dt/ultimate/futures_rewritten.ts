8a51b40edeff77a16905ad44d6595ac5552d9c05
Wexx <86693821+wexxlee@users.noreply.github.com>
2025-03-26 08:54:56 -0700

    lint fru

--- ui/raidboss/data/07-dt/ultimate/futures_rewritten.ts
+++ ui/raidboss/data/07-dt/ultimate/futures_rewritten.ts
@@ -2208,19 +2208,19 @@ const triggerSet: TriggerSet<Data> = {
             if (dpsWithTank === data.me)
               return {
                 alertText: output.towerYouSwap!({
-                  player: data.party.member(towerTank).toString(),
+                  player: data.party.member(towerTank),
                 }),
               };
             else if (towerTank === data.me)
               return {
                 alertText: output.towerYouSwap!({
-                  player: data.party.member(dpsWithTank).toString(),
+                  player: data.party.member(dpsWithTank),
                 }),
               };
             return {
               infoText: output.towerOtherSwap!({
-                p1: data.party.member(dpsWithTank).toString(),
-                p2: data.party.member(towerTank).toString(),
+                p1: data.party.member(dpsWithTank),
+                p2: data.party.member(towerTank),
               }),
             };
           }
@@ -2239,7 +2239,7 @@ const triggerSet: TriggerSet<Data> = {
             return defaultOutput;
           return {
             alertText: output.baitDPS!({
-              otherDps: data.party.member(otherDps).toString(),
+              otherDps: data.party.member(otherDps),
             }),
           };
         }
@@ -2307,7 +2307,7 @@ const triggerSet: TriggerSet<Data> = {
         const baitStackPlayer = data.p4DarklitStacks.find((p) => baitPlayers.includes(p));
         if (baitStackPlayer === undefined || !baitPlayers.includes(data.me))
           return {};
-        const stackName = data.party.member(baitStackPlayer).toString();
+        const stackName = data.party.member(baitStackPlayer);
 
         const isStackOnMe = data.me === baitStackPlayer;
         const defaultOutput = isStackOnMe ? { infoText: output.stackOnYou!() } : {};
