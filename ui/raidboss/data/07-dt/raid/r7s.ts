fc73b4dca34082c66402537cb90e48dd4a2aca1f
xiashtra <91220277+xiashtra@users.noreply.github.com>
2025-04-14 23:34:10 -0500

    Update r7s.ts

--- ui/raidboss/data/07-dt/raid/r7s.ts
+++ ui/raidboss/data/07-dt/raid/r7s.ts
@@ -1,8 +1,26 @@
+import Conditions from '../../../../../resources/conditions';
 import Outputs from '../../../../../resources/outputs';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
+
+// @TODO:
+// - Sinister Seeds - puddles
+// - Impact - healer stacks
+// - add interrupt calls?
+// - Hurricane Force - add enrage
+
+const headMarkerData = {
+  // Sinster Seeds marker, phase 1
+  'seedMarker1': '0177',
+  // Sinster Seeds marker, phase 2
+  'seedMarker2': '01D2',
+  // Pulp Smash stack marker
+  'stackMarker': '00A1',
+  // Abominable Blink flare marker
+  'flareMarker': '0147',
+} as const;
 
 export interface Data extends RaidbossData {
   brutalImpactCount: number;
@@ -20,7 +38,7 @@ const triggerSet: TriggerSet<Data> = {
       id: 'R7S Brutal Impact',
       type: 'StartsUsing',
       netRegex: { id: 'A55B', source: 'Brute Abombinator', capture: false },
-      alertText: (data, _matches, output) => output.text!({ count: data.brutalImpactCount }),
+      infoText: (data, _matches, output) => output.text!({ count: data.brutalImpactCount }),
       run: (data) => data.brutalImpactCount = Math.min(data.brutalImpactCount + 1, 8),
       outputStrings: {
         text: {
@@ -47,29 +65,42 @@ const triggerSet: TriggerSet<Data> = {
           const inOut = matches.id === 'A55F' ? output.in!() : output.out!();
           return output.sharedBuster!({ inOut: inOut });
         }
-        const inOut = matches.id === 'A55F' ? output.out!() : output.in!();
+        const inOut = matches.id === 'A560' ? output.in!() : output.out!();
         return output.avoidBuster!({ inOut: inOut });
       },
       outputStrings: {
         sharedBuster: {
-          en: '${inOut} => Shared tankbuster',
+          en: 'Tanks ${inOut} => Shared tankbuster',
         },
         avoidBuster: {
-          en: '${inOut} => Avoid tankbuster',
+          en: 'Party ${inOut} => Avoid tankbuster',
         },
         in: Outputs.in,
         out: Outputs.out,
       },
     },
     {
-      id: 'R7S Impact',
+      id: 'R7S Sinister Seeds',
       type: 'StartsUsing',
-      netRegex: { id: 'A574', source: 'Brute Abombinator', capture: false },
+      netRegex: { id: ['A56E', 'A598'], source: 'Brute Abombinator', capture: true },
+      condition: Conditions.targetIsYou(),
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
-        text: Outputs.healerGroups,
+        text: {
+          en: 'Drop seed',
+        },
       },
     },
+    // {
+    //   instant, not casted
+    //   id: 'R7S Impact',
+    //   type: 'StartsUsing',
+    //   netRegex: { id: 'A574', source: 'Brute Abombinator', capture: false },
+    //   alertText: (_data, _matches, output) => output.text!(),
+    //   outputStrings: {
+    //     text: Outputs.healerGroups,
+    //   },
+    // },
     {
       id: 'R7S Quarry Swamp',
       type: 'StartsUsing',
@@ -94,12 +125,12 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       id: 'R7S Pulp Smash',
-      type: 'StartsUsing',
-      netRegex: { id: 'A577', source: 'Brute Abombinator', capture: false },
-      infoText: (_data, _matches, output) => output.text!(),
+      type: 'HeadMarker',
+      netRegex: { id: headMarkerData.stackMarker, capture: true },
+      infoText: (_data, matches, output) => output.text!({ target: matches.target }),
       outputStrings: {
         text: {
-          en: 'Stack => Protean',
+          en: 'Stack on ${target} => Out + Protean',
         },
       },
     },
@@ -128,7 +159,7 @@ const triggerSet: TriggerSet<Data> = {
           case 'A58D':
             return output.out!();
           case 'A58F':
-            return output.out!();
+            return output.in!();
           case 'A591':
             return output.in!();
           case 'A5A2':
@@ -174,18 +205,41 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       id: 'R7S Thorny Deathmatch',
-      type: 'StartsUsing',
-      netRegex: { id: 'A588', source: 'Brute Abombinator', capture: true },
-      response: Responses.tankBuster(),
+      type: 'Tether',
+      netRegex: { id: '0152', capture: true },
+      infoText: (_data, matches, output) => output.text!({ target: matches.target }),
+      outputStrings: {
+        text: {
+          en: 'Tank tether on ${target}',
+        },
+      },
     },
     {
       id: 'R7S Abominable Blink',
+      type: 'HeadMarker',
+      netRegex: { id: headMarkerData.flareMarker, capture: true },
+      alertText: (data, matches, output) => {
+        if (matches.target === data.me)
+          return output.flare!();
+        return output.avoidFlare!();
+      },
+      outputStrings: {
+        avoidFlare: {
+          en: 'Away from Flare',
+        },
+        flare: {
+          en: 'Flare on YOU',
+        },
+      },
+    },
+    {
+      id: 'R7S Demolition Deathmatch',
       type: 'StartsUsing',
-      netRegex: { id: 'A589', source: 'Brute Abombinator', capture: false },
-      alertText: (_data, _matches, output) => output.text!(),
+      netRegex: { id: 'A596', source: 'Brute Abombinator', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
-          en: 'Away from Flare',
+          en: 'Get tethers',
         },
       },
     },
