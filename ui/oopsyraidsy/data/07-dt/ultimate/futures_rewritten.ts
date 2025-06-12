e4a3151311e1f9d4000498759fd9ae921a7ce661
Dowon <ks2515@naver.com>
2025-06-12 20:04:51 +0900

    fix: update to be more descriptive

--- ui/oopsyraidsy/data/07-dt/ultimate/futures_rewritten.ts
+++ ui/oopsyraidsy/data/07-dt/ultimate/futures_rewritten.ts
@@ -9,47 +9,47 @@ export type Data = OopsyData;
 const triggerSet: OopsyTriggerSet<Data> = {
   zoneId: ZoneId.FuturesRewrittenUltimate,
   damageWarn: {
-    'FRU Cyclonic Break': '9CD2', // P1 Cyclonic Break follow-up
-    'FRU Burnt Strike 1': '9CC1', // P1 Burnt Strike (fire, by Fatebreaker)
-    'FRU Burnt Strike 2': '9CC5', // P1 Burnt Strike (thunder, by Fatebreaker)
-    'FRU Burnt Strike 3': '9CE1', // P1 Burnt Strike (fire, by Fatebreaker's Image)
-    'FRU Burnt Strike 4': '9CE3', // P1 Burnt Strike (thunder, by Fatebreaker's Image)
-    'FRU Burnout 1': '9CC6', // P1 Burnt Strike thunder follow-up (by Fatebreaker)
-    'FRU Burnout 2': '9CE4', // P1 Burnt Strike thunder follow-up (by Fatebreaker's Image)
-    'FRU Blasting Zone': '9CDD', // P1 Blasting Zone
-    'FRU Brightfire 1': '9CD8', // P1 Brightfire
-    'FRU Brightfire 2': '9CD9', // P1 Brightfire
-    'FRU Icicle Impact': '9D06', // P2 Icicle Impact
-    'FRU Axe Kick': '9D0A', // P2 Axe Kick
-    'FRU Scythe Kick': '9D0B', // P2 Scythe Kick
-    'FRU Frigid Needle': '9D09', // P2 Frigid Needle
-    'FRU Twin Stillness 1': '9D01', // P2 Twin Stillness 1st
-    'FRU Twin Stillness 2': '9D04', // P2 Twin Stillness 2nd
-    'FRU Twin Silence 1': '9D02', // P2 Twin Silence 1st
-    'FRU Twin Silence 2': '9D03', // P2 Twin Silence 2nd
-    'FRU Reflected Scythe Kick 1': '9D0C', // P2 Reflected Scythe Kick
-    'FRU Reflected Scythe Kick 2': '9D0D', // P2 Reflected Scythe Kick
-    'FRU Explosion': '9D1B', // P2 Explosion from Holy Light
-    'FRU Hiemal Storm': '9D40', // P2.5 Crystal of Light puddle
-    'FRU Sinbound Blizzard III': '9D46', // P2.5 Crystal of Darkness cone
+    'FRU P1 Cyclonic Break': '9CD2', // P1 Cyclonic Break follow-up
+    'FRU P1 Burnt Strike Boss 1': '9CC1', // P1 Burnt Strike (fire, by Fatebreaker)
+    'FRU P1 Burnt Strike Boss 2': '9CC5', // P1 Burnt Strike (thunder, by Fatebreaker)
+    'FRU P1 Burnt Strike Clone 1': '9CE1', // P1 Burnt Strike (fire, by Fatebreaker's Image)
+    'FRU P1 Burnt Strike Clone 2': '9CE3', // P1 Burnt Strike (thunder, by Fatebreaker's Image)
+    'FRU P1 Burnout Thunder Boss': '9CC6', // P1 Burnt Strike thunder follow-up (by Fatebreaker)
+    'FRU P1 Burnout Thunder Clone': '9CE4', // P1 Burnt Strike thunder follow-up (by Fatebreaker's Image)
+    'FRU P1 Blasting Zone': '9CDD', // P1 Blasting Zone (Utopian Sky line AOE)
+    'FRU P1 Brightfire Puddle 1': '9CD8', // P1 Brightfire (fire, thunder puddle)
+    'FRU P1 Brightfire Puddle 2': '9CD9', // P1 Brightfire (fire, thunder puddle)
+    'FRU P2 Icicle Impact': '9D06', // P2 Icicle Impact
+    'FRU P2 Axe Kick': '9D0A', // P2 Axe Kick
+    'FRU P2 Scythe Kick': '9D0B', // P2 Scythe Kick
+    'FRU P2 Frigid Needle': '9D09', // P2 Frigid Needle (star-shaped AOE)
+    'FRU P2 Twin Stillness 1': '9D01', // P2 Twin Stillness 1st
+    'FRU P2 Twin Stillness 2': '9D04', // P2 Twin Stillness 2nd
+    'FRU P2 Twin Silence 1': '9D02', // P2 Twin Silence 1st
+    'FRU P2 Twin Silence 2': '9D03', // P2 Twin Silence 2nd
+    'FRU P2 Reflected Scythe Kick 1': '9D0C', // P2 Reflected Scythe Kick
+    'FRU P2 Reflected Scythe Kick 2': '9D0D', // P2 Reflected Scythe Kick
+    'FRU P2 Holy Light Explosion': '9D1B', // P2 Explosion from Holy Light
+    'FRU P2.5 Crystal of Light Hiemal Storm': '9D40', // P2.5 Crystal of Light puddle
+    'FRU P2.5 Crystal of Darkness Sinbound Blizzard III': '9D46', // P2.5 Crystal of Darkness cone
     'FRU Dark Blizzard III': '9D57', // P3, P4 Dark Blizzard III
-    'FRU Sinbound Meltdown 1': '9D64', // P3 Hourglass beam (after 1st hit)
-    'FRU Apocalypse': '9D69', // P3 Apocalypse
-    'FRU Akh Rhai': '9D2E', // P4 Akh Rhai
-    'FRU Hallowed Wings 1': '9D23', // P4 Dragonsong Hallowed Wings
-    'FRU Hallowed Wings 2': '9D24', // P4 Dragonsong Hallowed Wings
-    'FRU Maelstrom': '9D6B', // P4 Crystallize Time hourglass
-    'FRU Tidal Light 1': '9D3C', // P4 Crystallize Time exaflare
-    'FRU Tidal Light 2': '9D3D', // P4 Crystallize Time exaflare
-    'FRU Path of Light': '9D74', // P5 Fulgent Blade exaflare
-    'FRU Path of Darkness': '9D75', // P5 Fulgent Blade exaflare
-    'FRU Cruel Path of Light': '9CB7', // P5 Polarizing Paths follow-up
-    'FRU Cruel Path of Darkness': '9CB8', // P5 Polarizing Paths follow-up
+    'FRU P3 Hourglass Sinbound Meltdown After 1st Hit': '9D64', // P3 Hourglass beam (after 1st hit)
+    'FRU P3 Apocalypse': '9D69', // P3 Apocalypse
+    'FRU P4 Akh Rhai': '9D2E', // P4 Akh Rhai
+    'FRU P4 Hallowed Wings 1': '9D23', // P4 Dragonsong Hallowed Wings
+    'FRU P4 Hallowed Wings 2': '9D24', // P4 Dragonsong Hallowed Wings
+    'FRU P4 Hourglass Maelstrom': '9D6B', // P4 Crystallize Time hourglass
+    'FRU P4 Tidal Light Exaflare 1': '9D3C', // P4 Crystallize Time exaflare
+    'FRU P4 Tidal Light Exaflare 2': '9D3D', // P4 Crystallize Time exaflare
+    'FRU P5 Path of Light Exaflare': '9D74', // P5 Fulgent Blade exaflare
+    'FRU P5 Path of Darkness Exaflare': '9D75', // P5 Fulgent Blade exaflare
+    'FRU P5 Cruel Path of Light Polarizing Paths Follow-up': '9CB7', // P5 Polarizing Paths follow-up
+    'FRU P5 Cruel Path of Darkness Polarizing Paths Follow-up': '9CB8', // P5 Polarizing Paths follow-up
   },
   damageFail: {
-    'FRU Unmitigated Explosion 1': '9CC4', // P1 tower fail
-    'FRU Unmitigated Explosion 2': '9D81', // P5 tower fail
-    'FRU Refulgent Fate': '9D17', // P2, P4 tether break
+    'FRU P1 Unmitigated Explosion': '9CC4', // P1 tower fail
+    'FRU P5 Unmitigated Explosion': '9D81', // P5 tower fail
+    'FRU Refulgent Fate Tether Break': '9D17', // P2, P4 tether break
   },
   gainsEffectWarn: {
     'FRU Damage Down': 'B5F',
@@ -60,30 +60,30 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'FRU Mark of Mortality': '1114', // stack fail debuff
   },
   shareWarn: {
-    'FRU Sinsmite': '9CD5', // P1 spread during Cyclonic Break
-    'FRU Sinbound Thunder III': '9CE0', // P1 spread during Utopian Sky
-    'FRU Bow Shock': '9CCF', // P1 Bow Shock (tethered thunder)
-    'FRU House of Light 1': '9D0E', // P2 House of Light during Diamond Dust, Mirror Mirror
-    'FRU Banish III Divided': '9D1F', // P2 Banish III Divided (spread)
-    'FRU Sinbound Meltdown 2': '9D2B', // P3 Hourglass beam 1st hit
-    'FRU Dark Fire III': '9D54', // P3 Dark Fire III
+    'FRU P1 Sinsmite Spread': '9CD5', // P1 spread during Cyclonic Break
+    'FRU P1 Sinbound Thunder III Spread': '9CE0', // P1 spread during Utopian Sky
+    'FRU P1 Bow Shock': '9CCF', // P1 Bow Shock (tethered thunder)
+    'FRU P2 House of Light': '9D0E', // P2 House of Light during Diamond Dust, Mirror Mirror
+    'FRU P2 Banish III Divided Spread': '9D1F', // P2 Banish III Divided (spread)
+    'FRU P3 Sinbound Meltdown 1st Hit': '9D2B', // P3 Hourglass beam 1st hit
+    'FRU P3 Dark Fire III Marker': '9D54', // P3 Dark Fire III
     'FRU Dark Eruption': '9D52', // P3, P4 Dark Eruption
     'FRU Spirit Taker': '9D61', // P3, P4 Spirit Taker
-    'FRU Longing of the Lost': '9D31', // P4 Crystallize Time dragon explosion
+    'FRU P4 Drachen Wanderer Longing of the Lost': '9D31', // P4 Crystallize Time dragon explosion
   },
   shareFail: {
-    'FRU House of Light 2': '9CFC', // P2 House of Light after Light Rampant, P4 House of Light
-    'FRU Darkest Dance': '9CF6', // P3 Darkest Dance tank buster
-    'FRU Somber Dance 1': '9D5C', // P4 Somber Dance 1st
-    'FRU Somber Dance 2': '9D5D', // P4 Somber Dance 2nd
-    'FRU Wings Dark and Light 1': '9D7A', // P5 Wings Dark and Light cleave
-    'FRU Wings Dark and Light 2': '9D7B', // P5 Wings Dark and Light cleave
-    'FRU Wings Dark and Light 3': '9BC7', // P5 Wings Dark and Light tether
-    'FRU Wings Dark and Light 4': '9BC8', // P5 Wings Dark and Light tether
+    'FRU Lightsteeped House of Light': '9CFC', // P2 House of Light after Light Rampant, P4 House of Light
+    'FRU P3 Darkest Dance Tank Cleave': '9CF6', // P3 Darkest Dance tank buster
+    'FRU P4 Somber Dance 1st Farthest': '9D5C', // P4 Somber Dance 1st
+    'FRU P4 Somber Dance 2nd Nearest': '9D5D', // P4 Somber Dance 2nd
+    'FRU P5 Wings Dark and Light Cleave 1': '9D7A', // P5 Wings Dark and Light cleave
+    'FRU P5 Wings Dark and Light Cleave 2': '9D7B', // P5 Wings Dark and Light cleave
+    'FRU P5 Wings Dark and Light Tether 1': '9BC7', // P5 Wings Dark and Light tether
+    'FRU P5 Wings Dark and Light Tether 2': '9BC8', // P5 Wings Dark and Light tether
   },
   soloWarn: {
-    'FRU Sinsmoke': '9CD3', // P1 stack during Cyclonic Break
-    'FRU Banish III': '9D1E', // P2 Banish III (stack)
+    'FRU P1 Sinsmoke Stack': '9CD3', // P1 stack during Cyclonic Break
+    'FRU P2 Banish III Stack': '9D1E', // P2 Banish III (stack)
   },
   triggers: [
     {
