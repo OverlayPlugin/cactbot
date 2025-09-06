fb9f81bc0b8d9796c0278ff9e361010f5b79fad6
Legends0 <nac10fsu@gmail.com>
2025-09-06 16:44:40 -0400

    add tethers in Dead Stars

--- ui/raidboss/data/07-dt/eureka/occult_crescent_south_horn.ts
+++ ui/raidboss/data/07-dt/eureka/occult_crescent_south_horn.ts
@@ -1,11 +1,15 @@
 import Outputs from '../../../../../resources/outputs';
+import { callOverlayHandler } from '../../../../../resources/overlay_plugin_api';
 import { Responses } from '../../../../../resources/responses';
+import { Directions } from '../../../../../resources/util';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
-import { TriggerSet } from '../../../../../types/trigger';
+import { LocaleText, TriggerSet } from '../../../../../types/trigger';
 
 export interface Data extends RaidbossData {
   ce?: string;
+  deadStarsSnowballTetherDirNum?: number;
+  deadStarsSnowballTetherCount: number;
 }
 
 // List of events:
@@ -35,6 +39,25 @@ const ceIds: { [ce: string]: string } = {
   withExtremePredjudice: '339',
 };
 
+const headMarkerData = {
+  // Dead Stars boss tethers
+  'deadStarsBossTether': '00F9',
+  // Dead Stars boss tethers to each other
+  'deadStarsTether': '0136',
+  // Dead Stars distance-based tether
+  'deadStarsDistanceTether': '0001',
+} as const;
+
+// Occult Crescent Forked Tower: Blood Dead Stars consts
+const deadStarsFrozenPhobosLocaleNames: LocaleText = {
+  en: 'Frozen Phobos',
+};
+const deadStarsFrozenTritonLocaleNames: LocaleText = {
+  en: 'Frozen Triton',
+};
+const deadStarsCenterX = -800;
+const deadStarsCenterY = 360;
+
 const triggerSet: TriggerSet<Data> = {
   id: 'TheOccultCrescentSouthHorn',
   zoneId: ZoneId.TheOccultCrescentSouthHorn,
@@ -43,6 +66,9 @@ const triggerSet: TriggerSet<Data> = {
     cn: '蜃景幻界新月岛 南征之章 紧急遭遇战 触发器/时间轴。',
   },
   timelineFile: 'occult_crescent_south_horn.txt',
+  initData: () => ({
+    deadStarsSnowballTetherCount: 0,
+  }),
   resetWhenOutOfCombat: false,
   triggers: [
     {
@@ -186,6 +212,12 @@ const triggerSet: TriggerSet<Data> = {
       response: Responses.awayFromFront(),
     },
     {
+      id: 'Occult Crescent Demon Tablet Demonic Dark II',
+      type: 'StartsUsing',
+      netRegex: { source: 'Demon Tablet', id: 'A306', capture: false },
+      response: Responses.bigAoe(),
+    },
+    {
       id: 'Occult Crescent Demon Tablet Ray of Dangers Near/Expulsion Afar',
       // A2F3 Ray of Dangers Near
       // A2F4 Ray of Expulsion Afar
@@ -221,6 +253,86 @@ const triggerSet: TriggerSet<Data> = {
         },
       },
     },
+    {
+      id: 'Occult Crescent Dead Stars Boss Tether',
+      type: 'Tether',
+      netRegex: { id: [headMarkerData.deadStarsBossTether], capture: true },
+      condition: (data, matches) => {
+        // Do not execute in snowball phase
+        const frozenPhobosName = deadStarsFrozenPhobosLocaleNames[data.parserLang];
+        const frozenTritonName = deadStarsFrozenTritonLocaleNames[data.parserLang];
+        if (
+          data.me === matches.target && 
+          (matches.source !== frozenPhobosName &&
+          matches.source !== frozenTritonName)
+        )
+          return true;
+        return false;
+      },
+      infoText: (data, matches, output) => {
+        return output.boss!({ boss: matches.target });
+      },
+      outputStrings: {
+        boss: {
+          en: 'Tethered to ${boss}',
+        },
+      },
+    },
+    {
+      id: 'Occult Crescent Dead Stars Snowball Tether',
+      // Calls each tether or get towers
+      type: 'Tether',
+      netRegex: { id: [headMarkerData.deadStarsBossTether], capture: true },
+      condition: (data, matches) => {
+        // Only execute in snowball phase
+        const frozenPhobosName = deadStarsFrozenPhobosLocaleNames[data.parserLang];
+        const frozenTritonName = deadStarsFrozenTritonLocaleNames[data.parserLang];
+        if (matches.source === frozenPhobosName || matches.source === frozenTritonName)
+          return true;
+      },
+      preRun: (data, matches) => {
+        data.deadStarsSnowballTetherCount = data.deadStarsSnowballTetherCount + 1;
+      },
+      promise: async (data, matches) => {
+        // Only calculate direction for players that are targetted
+        if (data.me !== matches.target)
+          return;
+        const actors = (await callOverlayHandler({
+          call: 'getCombatants',
+          ids: [parseInt(matches.sourceId, 16)],
+        })).combatants;
+        const actor = actors[0];
+        if (actors.length !== 1 || actor === undefined) {
+          console.error(
+            `Occult Crescent Dead Stars Snowball Tether: Wrong actor count ${actors.length}`,
+          );
+          return;
+        }
+
+        const dirNum = Directions.xyTo8DirNum(actor.PosX, actor.PosY, deadStarsCenterX, deadStarsCenterY);
+        data.deadStarsSnowballTetherDirNum = (dirNum + 4) % 8;
+      },
+      infoText: (data, matches, output) => {
+        if (data.deadStarsSnowballTetherDirNum !== undefined && data.me === matches.target) {
+          // This will trigger for each tether a player has
+          const dir = output[Directions.outputFrom8DirNum(data.deadStarsSnowballTetherDirNum)]!();
+          return output.knockbackTetherDir!({ dir: dir });
+        }
+
+        // A player who has a tether should have a defined direction, but if they don't they'll get two calls
+        if (data.deadStarsSnowballTetherDirNum === undefined && data.deadStarsSnowballTetherCount === 2)
+          return output.knockbackToSnowball!();
+      },
+      outputStrings: {
+        ...Directions.outputStrings8Dir,
+        knockbackTetherDir: {
+          en: 'Tether: Knockback to ${dir} => To Wall',
+        },
+        knockbackToSnowball: {
+          en: 'Knockback to Snowball => To Wall',
+        },
+      },
+    },
   ],
   timelineReplace: [
     {
@@ -238,34 +350,24 @@ const triggerSet: TriggerSet<Data> = {
         'Clawmarks': '抓痕',
         'Cloister Demon': '回廊恶魔',
         'Crescent Berserker': '新月狂战士',
-        'Crystal Dragon': '水晶龙',
         'Death Claw': '死亡爪',
-        'Draconic Double': '水晶龙的幻影',
         'Hinkypunk': '鬼火苗',
         'Lion Rampant': '跃立狮',
         'Neo Garula': '进化加鲁拉',
-        'Nymian Petalodus': '尼姆瓣齿鲨',
         'Phantom Claw': '死亡爪的幻影',
-        'Repaired Lion': '复原狮像',
       },
       'replaceText': {
         '\\(in\\)': '(内)',
         '\\(jump\\)': '(跳)',
-        '\\(Lightning\\)': '(雷)',
         '\\(out\\)': '(外)',
-        '\\(Wind\\)': '(风)',
         'Bedrock Uplift': '地面隆起',
         'Blazing Flare': '炽热核爆',
         'Boil Over': '发怒',
         'Channeled Rage': '燥怒',
         'Clawing Shadow': '雾霾爪',
         'Clawmarks': '抓痕',
-        'Crystal Call': '生成晶石',
-        'Crystal Mirror': '转移晶石',
-        'Crystallized Energy': '水晶波动',
         'Dirty Nails': '腐坏爪',
         'Explosion': '爆炸',
-        'Fearsome Facet': '幻影晶石',
         'Gigaflare': '十亿核爆',
         'Great Ball of Fire': '火球',
         'Heated Outburst': '气焰',
@@ -274,10 +376,7 @@ const triggerSet: TriggerSet<Data> = {
         'Horizontal Crosshatch': '横向双重抓',
         'Karmic Drain': '生命侵蚀',
         'Lethal Nails': '死亡甲',
-        'Made Magic': '释放魔力',
         'Manifold Marks': '多重抓痕',
-        'Primal Roar': '大咆哮',
-        'Prismatic Wing': '水晶之翼',
         'Raking Scratch': '尖甲疾袭',
         'Scathing Sweep': '横砍',
         'Seal Asunder': '封印破坏',
