7d8b1345225c9810f6b3b5b94662e32f3ee75d4f
Legends0 <nac10fsu@gmail.com>
2025-10-07 21:02:56 -0400

    comment update

--- ui/raidboss/data/07-dt/eureka/occult_crescent_south_horn.ts
+++ ui/raidboss/data/07-dt/eureka/occult_crescent_south_horn.ts
@@ -131,7 +131,7 @@ const triggerSet: TriggerSet<Data> = {
       // Berserker = 2
       // Knight = 1
       // Freelancer = null
-      // Freelancer level is accumulation of maxed jobs and seems like only 3 more jobs, ending at F
+      // Freelancer level is accumulation of maxed jobs +1 and seems like only 3 more jobs, ending at F
       type: 'GainsEffect',
       netRegex: { effectId: [...phantomJobEffectIds], capture: true },
       condition: Conditions.targetIsYou(),
