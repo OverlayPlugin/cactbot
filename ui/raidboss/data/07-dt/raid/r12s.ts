6ce0c4a073ad80d302317dc08608920c1b68af46
Legends0 <nac10fsu@gmail.com>
2026-02-26 00:25:25 -0500

    shorten p1 slaughtershed blob call

--- ui/raidboss/data/07-dt/raid/r12s.ts
+++ ui/raidboss/data/07-dt/raid/r12s.ts
@@ -2234,8 +2234,8 @@ const triggerSet: TriggerSet<Data> = {
         northwestKnockback: {
           en: 'Knockback from Northwest',
         },
-        northeast: Outputs.northeast,
-        northwest: Outputs.northwest,
+        northeast: Outputs.dirNE,
+        northwest: Outputs.dirNW,
         stackOnYou: Outputs.stackOnYou,
         stackOnPlayer: Outputs.stackOnPlayer,
         stackDir: {
@@ -2287,8 +2287,8 @@ const triggerSet: TriggerSet<Data> = {
         northwestKnockback: {
           en: 'Knockback from Northwest',
         },
-        northeast: Outputs.northeast,
-        northwest: Outputs.northwest,
+        northeast: Outputs.dirNE,
+        northwest: Outputs.dirNW,
         spread: Outputs.spread,
         spreadDir: {
           en: 'Spread ${dir}',
