a38a94706bc08c4bd1d89e8a72cd0ed9f50abe4e
valarnin <valarnin@gmail.com>
2026-04-29 20:41:14 -0400

    raidboss: The Unmaking (Normal)

---
+++ ui/raidboss/data/07-dt/trial/enuo.ts
@@ -0,0 +1,303 @@
+import Conditions from '../../../../../resources/conditions';
+import Outputs from '../../../../../resources/outputs';
+import { Responses } from '../../../../../resources/responses';
+import { DirectionOutputCardinal, Directions } from '../../../../../resources/util';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export interface Data extends RaidbossData {
+  gazeDir: 'CW' | 'CCW';
+  naughtGrowsPositions: (DirectionOutputCardinal | 'middle')[];
+  flareTargets: string[];
+}
+
+const center = {
+  x: 100,
+  y: 100,
+} as const;
+
+const triggerSet: TriggerSet<Data> = {
+  id: 'TheUnmaking',
+  zoneId: ZoneId.TheUnmaking,
+  timelineFile: 'enuo.txt',
+  initData: () => ({
+    naughtGrowsPositions: [],
+    gazeDir: 'CW',
+    flareTargets: [],
+  }),
+  triggers: [
+    {
+      id: 'Enuo Meteorain',
+      type: 'StartsUsing',
+      netRegex: { id: 'C333', source: 'Enuo', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Enuo Almagest',
+      type: 'StartsUsing',
+      netRegex: { id: 'C308', source: 'Enuo', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Enuo Lightless World',
+      type: 'StartsUsing',
+      netRegex: { id: 'C327', source: 'Enuo', capture: false },
+      response: Responses.bigAoe(),
+    },
+
+    {
+      id: 'Enuo Naught Grows',
+      type: 'StartsUsingExtra',
+      netRegex: { id: ['C30D', 'C30E'], capture: true },
+      preRun: (data, matches) => {
+        let dir: DirectionOutputCardinal | 'middle';
+
+        const x = parseFloat(matches.x);
+        const y = parseFloat(matches.y);
+
+        if (Math.abs(x - center.x) < 1 && Math.abs(y - center.y) < 1) {
+          dir = 'middle';
+        } else {
+          dir = Directions.xyToCardinalDirOutput(x, y, center.x, center.y);
+        }
+        data.naughtGrowsPositions.push(dir);
+      },
+      delaySeconds: 0.5,
+      infoText: (data, _matches, output) => {
+        if (data.naughtGrowsPositions.length === 0)
+          return;
+
+        const [pos1, pos2] = data.naughtGrowsPositions;
+        // Clear data here instead of in run due to variable count of entries
+        data.naughtGrowsPositions = [];
+
+        if (pos1 === undefined)
+          return;
+
+        // Three possible patterns: Single, Double with one under boss, Double at opposite sides
+        if (pos2 === undefined)
+          return output.awayFrom!({ dir: output[pos1]!() });
+
+        if (pos1 === 'middle' || pos2 === 'middle') {
+          const awayFromDir = pos1 === 'middle' ? pos2 : pos1;
+          const dirOutput = output[awayFromDir]!();
+          return output.awayFromAndOut!({ dir: dirOutput });
+        }
+
+        const [dir1, dir2] = Directions.outputCardinalDir.filter((dir) =>
+          ![pos1, pos2].includes(dir)
+        );
+        if (dir1 === undefined || dir2 === undefined)
+          return;
+        const dir1Output = output[dir1]!();
+        const dir2Output = output[dir2]!();
+        return output.goDirections!({ dir1: dir1Output, dir2: dir2Output });
+      },
+      outputStrings: {
+        ...Directions.outputStringsCardinalDir,
+        middle: Outputs.middle,
+        awayFrom: {
+          en: 'Away from ${dir}',
+        },
+        awayFromAndOut: {
+          en: 'Away from ${dir} + Out',
+        },
+        goDirections: {
+          en: 'Go ${dir1}/${dir2} + Max Melee',
+        },
+      },
+    },
+    {
+      id: 'Enuo Gaze of the Void CW',
+      type: 'StartsUsing',
+      netRegex: { id: 'C31F', source: 'Enuo', capture: false },
+      run: (data) => data.gazeDir = 'CW',
+    },
+    {
+      id: 'Enuo Gaze of the Void CCW',
+      type: 'StartsUsing',
+      netRegex: { id: 'C320', source: 'Enuo', capture: false },
+      run: (data) => data.gazeDir = 'CCW',
+    },
+    {
+      id: 'Enuo Gaze of the Void',
+      type: 'StartsUsingExtra',
+      netRegex: { id: 'C321', capture: true },
+      delaySeconds: 0.2,
+      suppressSeconds: 30,
+      infoText: (data, matches, output) => {
+        const coneDir = Directions.hdgTo8DirNum(parseFloat(matches.heading));
+        let startDir;
+        let endDir;
+        if (data.gazeDir === 'CW') {
+          startDir = ((coneDir + 8) - 1) % 8;
+          endDir = (coneDir + 1) % 8;
+        } else {
+          startDir = (coneDir + 1) % 8;
+          endDir = ((coneDir + 8) - 1) % 8;
+        }
+        return output.text!({
+          rotation: output[data.gazeDir]!(),
+          dir1: output[Directions.output8Dir[startDir] ?? 'unknown']!(),
+          dir2: output[Directions.output8Dir[endDir] ?? 'unknown']!(),
+        });
+      },
+      outputStrings: {
+        ...Directions.outputStrings8Dir,
+        CW: Outputs.clockwise,
+        CCW: Outputs.counterclockwise,
+        text: {
+          en: '${dir1} ${rotation} => ${dir2}',
+        },
+      },
+    },
+    {
+      id: 'Enuo Deep Freeze',
+      type: 'StartsUsing',
+      netRegex: { id: 'C32E', source: 'Enuo', capture: true },
+      preRun: (data, matches) => {
+        data.flareTargets.push(matches.target);
+      },
+      delaySeconds: 0.1,
+      infoText: (data, _matches, output) => {
+        if (data.flareTargets.length < 2)
+          return;
+        if (data.flareTargets.includes(data.me))
+          return output.tankFlareOnYou!();
+        return output.awayFromFlares!();
+      },
+      run: (data) => {
+        data.flareTargets = [];
+      },
+      outputStrings: {
+        tankFlareOnYou: {
+          en: 'Tank Flare on YOU',
+        },
+        awayFromFlares: {
+          en: 'Away from tank flares',
+        },
+      },
+    },
+    {
+      id: 'Enuo Shrouded Holy',
+      type: 'StartsUsing',
+      netRegex: { id: 'C32F', source: 'Enuo', capture: true },
+      response: Responses.stackMarkerOn(),
+    },
+    {
+      id: 'Enuo Dimension Zero',
+      type: 'StartsUsing',
+      netRegex: { id: 'C331', source: 'Enuo', capture: true },
+      response: Responses.stackMarkerOn(),
+    },
+    {
+      id: 'Enuo Naught Hunts Tether',
+      type: 'Tether',
+      netRegex: { id: '0194', capture: true },
+      condition: Conditions.targetIsYou(),
+      infoText: (_data, _matches, output) => output.chasingPuddle!(),
+      outputStrings: {
+        chasingPuddle: {
+          en: 'Chasing puddle on you',
+        },
+      },
+    },
+    {
+      id: 'Enuo Naught Hunts Another Tether',
+      type: 'Tether',
+      netRegex: { id: '0195', capture: true },
+      condition: Conditions.targetIsYou(),
+      infoText: (_data, _matches, output) => output.chasingPuddle!(),
+      outputStrings: {
+        chasingPuddle: {
+          en: 'Chasing puddle on you',
+        },
+      },
+    },
+    {
+      id: 'Enuo Meltdown',
+      type: 'StartsUsing',
+      netRegex: { id: 'C32A', source: 'Enuo', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Bait puddles => spread',
+        },
+      },
+    },
+    {
+      id: 'Enuo Silent Torrent',
+      type: 'StartsUsingExtra',
+      netRegex: { id: 'C317', capture: true },
+      infoText: (_data, matches, output) => {
+        // C317 is the short line. Safe spots are (clockwise):
+        // +6 far
+        // +7 medium
+        // +8 short
+        // +9 short
+        // +10 short
+        // +11 medium
+        // +12 far
+
+        // For brevity, call +6/+12 far and +9 short only
+        const x = parseFloat(matches.x);
+        const y = parseFloat(matches.y);
+        const dangerDirNum = Directions.xyTo16DirNum(x, y, center.x, center.y);
+        const safeFar1DirNum = Directions.output16Dir[(dangerDirNum + 6) % 16] ?? 'unknown';
+        const safeFar2DirNum = Directions.output16Dir[(dangerDirNum + 12) % 16] ?? 'unknown';
+        const closeDirNum = Directions.output16Dir[(dangerDirNum + 9) % 16] ?? 'unknown';
+
+        return output.text!({
+          dir1: output[safeFar1DirNum]!(),
+          dir2: output[safeFar2DirNum]!(),
+          dir3: output[closeDirNum]!(),
+        });
+      },
+      outputStrings: {
+        ...Directions.outputStrings8Dir,
+        ...Directions.outputStrings16Dir,
+        text: {
+          en: '${dir1}/${dir2} Far / ${dir3} Close',
+        },
+      },
+    },
+    {
+      id: 'Enuo Looming Emptiness',
+      type: 'StartsUsing',
+      netRegex: { id: 'C33D', source: 'Looming Shadow', capture: false },
+      infoText: (_data, _matches, output) => output.away!(),
+      outputStrings: {
+        away: {
+          en: 'Away from proximity marker',
+        },
+      },
+    },
+    {
+      id: 'Enuo Add Spawn',
+      type: 'NameToggle',
+      netRegex: { name: 'Looming Shadow', toggle: '01', capture: false },
+      suppressSeconds: 9999,
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Kill adds',
+        },
+      },
+    },
+    {
+      id: 'Enuo Beacon Spawn',
+      type: 'MapEffect',
+      netRegex: { flags: '00020001', location: '03', capture: false },
+      suppressSeconds: 9999,
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Kill light beacon',
+        },
+      },
+    },
+  ],
+};
+
+export default triggerSet;
