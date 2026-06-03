1a77e922c8f7e3885fbbd255cec0af3763416bc8
ForestSageSarah <SarahCopelandX@icloud.com>
2026-06-03 13:41:47 -0400

    raidboss: add Pari basic ability calls

--- ui/raidboss/data/07-dt/dungeon/the_merchants_tale_advanced.ts
+++ ui/raidboss/data/07-dt/dungeon/the_merchants_tale_advanced.ts
@@ -17,6 +17,38 @@ const triggerSet: TriggerSet<Data> = {
       netRegex: { source: 'Pari of Plenty', id: 'B1CC', capture: false },
       response: Responses.aoe(),
     },
+    {
+      id: 'Pari Fire Of Victory',
+      type: 'StartsUsing',
+      netRegex: { source: 'Pari of Plenty', id: 'B1CE' },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'Pari Scouring Scorn',
+      type: 'StartsUsing',
+      netRegex: { source: 'Pari of Plenty', id: 'B1B2', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Pari Doubling Center For Chains',
+      type: 'StartsUsing',
+      netRegex: { source: 'Pari of Plenty', id: 'B093', capture: false },
+      durationSeconds: 4,
+      alertText: (_data, _matches, output) => output.centerForChains!(),
+      outputStrings: {
+        centerForChains: { en: 'Center for Chains' },
+      },
+    },
+    {
+      id: 'Pari Sun Circlet',
+      type: 'StartsUsing',
+      netRegex: { source: 'Pari of Plenty', id: 'B187', capture: false },
+      suppressSeconds: 3,
+      alarmText: (_data, _matches, output) => output.inBossHitbox!(),
+      outputStrings: {
+        inBossHitbox: { en: 'In Boss Hitbox' },
+      },
+    },
   ],
 };
 
