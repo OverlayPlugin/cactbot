83f0c7d998791f353051c4289730072848e350b9
Legends0 <nac10fsu@gmail.com>
2026-06-03 02:04:48 -0400

    add initial triggers

--- ui/raidboss/data/07-dt/ultimate/dancing_mad.ts
+++ ui/raidboss/data/07-dt/ultimate/dancing_mad.ts
@@ -1,6 +1,8 @@
+import Outputs from '../../../../../resources/outputs';
+import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
-import { TriggerSet } from '../../../../../types/trigger';
+import { OutputStrings, TriggerSet } from '../../../../../types/trigger';
 
 type Phase = 'p1' | 'p2';
 const phases: { [id: string]: Phase } = {
@@ -13,7 +15,111 @@ const phases: { [id: string]: Phase } = {
 export interface Data extends RaidbossData {
   // General
   phase: Phase | 'unknown';
+  // Phase 1
+  fireMarker?: string;
+  isFireTrue?: boolean;
+  isIceTrue?: boolean;
+  isThunderTrue?: boolean;
 }
+
+const headMarkerData = {
+  // Phase 1 Boss
+  'fakeFire': '02A1',
+  'trueFire': '02A2',
+  'fakeIce': '02A3',
+  'trueIce': '02A4',
+  'fakeThunder': '02A5',
+  'trueThunder': '02A6',
+  // Phase 1 Players
+  'tankbuster': '00DA', // Revolting Ruin III tankbuster
+  'dorito': '007F', // spread (real) or stack (fake)
+  'stack': '0080', // spread (fake) or stack (real)
+} as const;
+
+const mysteryMagicOutputStrings: OutputStrings = {
+  spread: Outputs.spread,
+  stack: {
+    en: 'Stack',
+    de: 'Stacken',
+    fr: 'Packez-vous',
+    ja: 'スタック',
+    cn: '集合',
+    ko: '집합',
+    tc: '集合',
+  },
+  trueThunder: {
+    en: 'True Thunder',
+    de: 'Wahrer Blitz',
+    fr: 'Vraie foudre',
+    ja: '真サンダガ',
+    cn: '真雷',
+    ko: '진실 선더가',
+    tc: '真雷',
+  },
+  fakeThunder: {
+    en: 'Fake Thunder',
+    de: 'Falscher Blitz',
+    fr: 'Fausse foudre',
+    ja: 'にせサンダガ',
+    cn: '假雷',
+    ko: '거짓 선더가',
+    tc: '假雷',
+  },
+  trueIce: {
+    en: 'True Ice',
+    de: 'Wahres Eis',
+    fr: 'Vraie glace',
+    ja: '真ブリザガ',
+    cn: '真冰',
+    ko: '진실 블리자가',
+    tc: '真冰',
+  },
+  fakeIce: {
+    en: 'Fake Ice',
+    de: 'Falsches Eis',
+    fr: 'Fausse glace',
+    ja: 'にせブリザガ',
+    cn: '假冰',
+    ko: '거짓 블리자가',
+    tc: '假冰',
+  },
+  stackTrueIce: {
+    en: '${mech} + ${ice}',
+  },
+  stackFakeIce: {
+    en: '${mech} + ${ice}',
+  },
+  spreadTrueIce: {
+    en: '${mech} + ${ice}',
+  },
+  spreadFakeIce: {
+    en: '${mech} + ${ice}',
+  },
+  trueIceTrueThunder: {
+    en: '${ice} + ${thunder}',
+  },
+  fakeIceTrueThunder: {
+    en: '${ice} + ${thunder}',
+  },
+  trueIceFakeThunder: {
+    en: '${ice} + ${thunder}',
+  },
+  fakeIceFakeThunder: {
+    en: '${ice} + ${thunder}',
+  },
+  stackTrueThunder: {
+    en: '${mech} + ${thunder}',
+  },
+  stackFakeThunder: {
+    en: '${mech} + ${thunder}',
+  },
+  spreadTrueThunder: {
+    en: '${mech} + ${thunder}',
+  },
+  spreadFakeThunder: {
+    en: '${mech} + ${thunder}',
+  },
+};
 
 const triggerSet: TriggerSet<Data> = {
   id: 'DancingMadUltimate',
@@ -30,6 +136,198 @@ const triggerSet: TriggerSet<Data> = {
       type: 'StartsUsing',
       netRegex: { id: Object.keys(phases) },
       run: (data, matches) => data.phase = phases[matches.id] ?? 'unknown',
+    },
+    {
+      id: 'DMU P1 Revolting Ruin III',
+      // Tankbuster targets highest enmity then the nearest player that is not the highest enmity
+      // Offtank can provoke to cause the main tank to take both hits so long as main tank is closest
+      type: 'HeadMarker',
+      netRegex: { id: headMarkerData['tankbuster'], capture: true },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'DMU P1 Mystery Magic Collect',
+      type: 'HeadMarker',
+      netRegex: {
+        id: [
+          headMarkerData['trueFire'],
+          headMarkerData['trueIce'],
+          headMarkerData['trueThunder'],
+          headMarkerData['fakeFire'],
+          headMarkerData['fakeIce'],
+          headMarkerData['fakeThunder'],
+        ],
+        capture: true,
+      },
+      run: (data, matches) => {
+        switch (matches.id) {
+          case headMarkerData['trueFire']:
+            data.isFireTrue = true;
+            return;
+          case headMarkerData['fakeFire']:
+            data.isFireTrue = false;
+            return;
+          case headMarkerData['trueIce']:
+            data.isIceTrue = true;
+            return;
+          case headMarkerData['fakeIce']:
+            data.isIceTrue = false;
+            return;
+          case headMarkerData['trueThunder']:
+            data.isThunderTrue = true;
+            return;
+          case headMarkerData['fakeThunder']:
+            data.isThunderTrue = false;
+            return;
+        }
+      },
+    },
+    {
+      id: 'DMU P1 Fire Head Marker Collect',
+      type: 'HeadMarker',
+      netRegex: { id: [headMarkerData['dorito'], headMarkerData['stack']], capture: true },
+      suppressSeconds: 2,
+      run: (data, matches) => data.fireMarker = matches.id,
+    },
+    {
+      id: 'DMU P1 Mystery Magic Ice and Fire',
+      // Set 1: Only Ice and Fire should be set
+      type: 'StartsUsing',
+      netRegex: { id: 'BA94', source: 'Kefka', capture: false },
+      condition: (data) => {
+        return data.isIceTrue !== undefined && data.isFireTrue !== undefined;
+      },
+      infoText: (data, _matches, output) => {
+        const fireMarker = data.fireMarker;
+        if (
+          (fireMarker === headMarkerData['dorito'] && data.isFireTrue) ||
+          (fireMarker === headMarkerData['stack'] && !data.isFireTrue)
+        )
+          return data.isIceTrue
+            ? output.spreadTrueIce!({ mech: output.spread!(), ice: output.trueIce!() })
+            : output.spreadFakeIce!({ mech: output.spread!(), ice: output.fakeIce!() });
+
+        if (
+          (fireMarker === headMarkerData['dorito'] && !data.isFireTrue) ||
+          (fireMarker === headMarkerData['stack'] && data.isFireTrue)
+        ) {
+          return data.isIceTrue
+            ? output.stackTrueIce!({ mech: output.stack!(), ice: output.trueIce!() })
+            : output.stackFakeIce!({ mech: output.stack!(), ice: output.fakeIce!() });
+        }
+      },
+      outputStrings: mysteryMagicOutputStrings,
+    },
+    {
+      id: 'DMU P1 Mystery Magic Ice and Thunder',
+      // Set 2: Only Ice and Thunder should be set
+      type: 'StartsUsing',
+      netRegex: { id: 'BA94', source: 'Kefka', capture: false },
+      condition: (data) => {
+        return data.isIceTrue !== undefined && data.isThunderTrue !== undefined;
+      },
+      infoText: (data, _matches, output) => {
+        if (data.isThunderTrue) {
+          return data.isIceTrue
+            ? output.trueIceTrueThunder!({
+              ice: output.trueIce!(),
+              thunder: output.trueThunder!(),
+            })
+            : output.fakeIceTrueThunder!({
+              ice: output.fakeIce!(),
+              thunder: output.trueThunder!(),
+            });
+        }
+        return data.isIceTrue
+          ? output.trueIceTrueThunder!({
+            ice: output.trueIce!(),
+            thunder: output.fakeThunder!(),
+          })
+          : output.fakeIceFakeThunder!({
+            ice: output.fakeIce!(),
+            thunder: output.fakeThunder!(),
+          });
+      },
+      outputStrings: mysteryMagicOutputStrings,
+    },
+    {
+      id: 'DMU P1 Mystery Magic Fire and Thunder',
+      // Set 2: Only Ice and Thunder should be set
+      type: 'StartsUsing',
+      netRegex: { id: 'BA94', source: 'Kefka', capture: false },
+      condition: (data) => {
+        return data.isFireTrue !== undefined && data.isThunderTrue !== undefined;
+      },
+      infoText: (data, _matches, output) => {
+        const fireMarker = data.fireMarker;
+        if (
+          (fireMarker === headMarkerData['dorito'] && data.isFireTrue) ||
+          (fireMarker === headMarkerData['stack'] && !data.isFireTrue)
+        )
+          return data.isThunderTrue
+            ? output.spreadTrueThunder!({
+              mech: output.spread!(),
+              thunder: output.trueThunder!(),
+            })
+            : output.spreadFakeThunder!({
+              mech: output.spread!(),
+              thunder: output.fakeThunder!(),
+            });
+
+        if (
+          (fireMarker === headMarkerData['dorito'] && !data.isFireTrue) ||
+          (fireMarker === headMarkerData['stack'] && data.isFireTrue)
+        ) {
+          return data.isThunderTrue
+            ? output.stackTrueThunder!({
+              mech: output.stack!(),
+              thunder: output.trueThunder!(),
+            })
+            : output.stackFakeThunder!({
+              mech: output.stack!(),
+              thunder: output.fakeThunder!(),
+            });
+        }
+      },
+      outputStrings: mysteryMagicOutputStrings,
+    },
+    {
+      id: 'DMU P1 Mystery Magic Cleanup',
+      // C622 Light of Judgment to reset for the Graven Image 2
+      type: 'StartsUsing',
+      netRegex: { id: ['BA94', 'C622'], source: 'Kefka', capture: false },
+      run: (data) => {
+        delete data.isFireTrue;
+        delete data.isIceTrue;
+        delete data.isThunderTrue;
+        delete data.fireMarker;
+      },
+    },
+    {
+      id: 'DMU P1 Light of Judgment',
+      type: 'StartsUsing',
+      netRegex: { id: 'C622', source: 'Kefka', capture: false },
+      response: Responses.bigAoe(),
+    },
+    {
+      id: 'DMU P1 Hyperdrive',
+      // This hits three times
+      type: 'StartsUsing',
+      netRegex: { id: 'C24B', source: 'Kefka' },
+      suppressSeconds: 5,
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'DMU P1 Intemperate Will',
+      type: 'StartsUsing',
+      netRegex: { id: 'BAB2', source: 'Graven Image', capture: false },
+      response: Responses.goWest(),
+    },
+    {
+      id: 'DMU P1 Gravitational Wave',
+      type: 'StartsUsing',
+      netRegex: { id: 'BAB1', source: 'Graven Image', capture: false },
+      response: Responses.goEast(),
     },
   ],
   timelineReplace: [
