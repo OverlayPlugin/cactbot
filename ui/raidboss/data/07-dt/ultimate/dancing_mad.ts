af93d851a52d40d6f3b91d07383dd3fb8dea343f
Legends0 <nac10fsu@gmail.com>
2026-06-11 22:53:26 -0400

    lint + missing delayseconds for umbra smash

--- ui/raidboss/data/07-dt/ultimate/dancing_mad.ts
+++ ui/raidboss/data/07-dt/ultimate/dancing_mad.ts
@@ -1232,7 +1232,9 @@ const triggerSet: TriggerSet<Data> = {
         if (data.isFireShort === undefined) {
           const isShort = parseFloat(matches.duration) < 21;
           data.isFireShort = (isShort && id === '640') ||
-            (!isShort && id === '641') ? true : false;
+            (!isShort && id === '641')
+          ? true
+          : false;
         }
         if (data.me === matches.target)
           data.myElement = id === '640' ? 'fire' : 'water';
@@ -1401,6 +1403,7 @@ const triggerSet: TriggerSet<Data> = {
       // BAFE  Latitudinal Implosion
       type: 'Ability',
       netRegex: { id: ['BAFD', 'BAFE'], source: 'Chaos', capture: false },
+      delaySeconds: 10,
       suppressSeconds: 99999,
       infoText: (_data, _matches, output) => output.bait!(),
       outputStrings: {
